6218e24f4fdbebfb54d1f18759aa63b9fd4c75be
Andriy Petlovanyy <andriy@noemail.com>
2020-10-04 22:21:08 +0000

    Added test for circular buffer

--- carmen_hardware/firmware/test/test_circular_buffer.cpp
+++ carmen_hardware/firmware/test/test_circular_buffer.cpp
@@ -25,7 +25,8 @@ TEST(TestSuite, parametersValidation)
   circular_buff_struct.buffer_size = 10;
   circular_buff_struct.p_buffer = NULL;
 
-  ON_GLOBAL_CALL(generate_error, generate_error(ERROR_SOFTWARE, NotNull(), Gt(0))).WillByDefault(Throw(std::exception()));
+  ON_GLOBAL_CALL(generate_error, generate_error(ERROR_SOFTWARE, NotNull(), Gt(0))).WillByDefault(Throw(
+    std::exception()));
   ASSERT_ANY_THROW(circular_buffer_add(NULL, NULL, 10));
   ASSERT_ANY_THROW(circular_buffer_add(NULL, buffer, buffer_length));
   ASSERT_ANY_THROW(circular_buffer_add(&circular_buff_struct, buffer, buffer_length));
@@ -38,6 +39,34 @@ TEST(TestSuite, parametersValidation)
   circular_buff_struct.buffer_size = 0;
   ASSERT_ANY_THROW(circular_buffer_add(&circular_buff_struct, buffer, buffer_length));
   ASSERT_ANY_THROW(circular_buffer_dequeue(&circular_buff_struct, buffer, buffer_length));
+}
+
+
+TEST(TestSuite, happyPath)
+{
+  uint8_t buffer[] = "Hello from test";
+  const uint32_t circular_buffer_length = 2 * sizeof(buffer);
+  uint8_t circular_buffer[circular_buffer_length] = { 0 };
+  uint8_t output_buffer[circular_buffer_length] = { 0 };
+
+  circular_buffer_t circular_buff_struct;
+  circular_buff_struct.head_index = 0;
+  circular_buff_struct.tail_index = 0;
+  circular_buff_struct.buffer_size = circular_buffer_length;
+  circular_buff_struct.p_buffer = circular_buffer;
+
+  ON_GLOBAL_CALL(generate_error, generate_error(ERROR_SOFTWARE, NotNull(), Gt(0))).WillByDefault(Throw(
+    std::exception()));
+  circular_buffer_add(&circular_buff_struct, buffer, sizeof(buffer));
+  ASSERT_EQ(circular_buff_struct.tail_index - circular_buff_struct.head_index, sizeof(buffer));
+  ASSERT_STREQ((char*)circular_buffer, (char*)buffer);
+
+  uint32_t actual_size = circular_buffer_dequeue(&circular_buff_struct, output_buffer, sizeof(buffer) + 2);
+  ASSERT_EQ(circular_buff_struct.tail_index, circular_buff_struct.head_index);
+  ASSERT_STREQ((char*)output_buffer, (char*)buffer);
+  ASSERT_EQ(sizeof(buffer), actual_size);
+
+
 }
 
 int main(int argc, char **argv)
